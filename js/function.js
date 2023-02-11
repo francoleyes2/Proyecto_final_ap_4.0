da675234c5b6a9a8489a8c5558cb25ea4926c025
francoleyes2 <francoleyes2@gmail.com>
2023-02-10 22:32:31 -0300

    Version.a01

--- js/function.js
+++ js/function.js
@@ -23,9 +23,61 @@ function mostrar_mensaje_t() {
   };
 
 
-  document.getElementById('boton_ocultar').addEventListener('click', function() {
+  document.getElementById('boton_ocultar').addEventListener('click', function(){
     document.getElementById('text-v').style.display ='none';
     });
+
+
+
+
+
+
+const txt = '{"nombre":"Franco", "edad":21, "ciudad":"Buenos Aires"}'
+const obj = JSON.parse(txt);
+document.getElementById("json-01").innerHTML = obj.nombre + ", " + obj.edad + ", " + obj.ciudad + ", ";
+
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+
+
+
+
+ 
+
+  function clickCounter() {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 0;
+    }
+    document.getElementById("clickk").innerHTML = localStorage.clickcount;
+  }
+
+
+
+
+
+
+  var x = document.getElementById("geo-01");
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+  }
 
 
 
